c3831f24f9440e754cb287c32ac7af75b27b45ea
AlexSytsenko <sytsenko@gmail.com>
2021-03-18 18:45:43 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React from 'react';
-import Painting from './components/Painting';
+import Logo from './components/Logo';
+import PaintingList from './components/PaintingList';
+import Panel from './components/Panel';
 import paintings from './paintings.json';
-
-// const painting = {
-//   "id": "id-1",
-//   "url": "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
-//   "title": "Feathers. Art abstract",
-//   "price": 500,
-//   "author": {
-//     "tag": "ractapopulous",
-//     "url": "https://pixabay.com/users/ractapopulous-24766/"
-//   },
-//   "quantity": 10
-// };
-
 
 const App = () => {
 
   return (
     <div>
-      <h1>Главный компонент</h1>
-      <Painting
-        // url={paintings[0].url}
-        title={paintings[0].author.tag}
-        price={paintings[0].price}
-        profileUrl={paintings[0].author.url}
-        tag={paintings[0].author.tag}
-        quantity={paintings[0].quantity} />
+      <Panel title="Свежие новости">
+        <p>lorem20asdffffffffffsdfasdfasdf dfsdafasdf asdf asdfadfadfadf dfdf    dfsf</p>
+        <a href=""></a>
+      </Panel>
+      <Panel>
+        <p>фівлдаофждвілаофд ловафдао вдфілао длфова</p></Panel>
+      <Logo text="Главный компонент" />
+      <PaintingList paintings={paintings}/>
     </div>
   );
 };
