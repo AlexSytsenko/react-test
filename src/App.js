904ae8666dd46a49c47035f7f5a4e49810b74407
AlexSytsenko <sytsenko@gmail.com>
2021-03-21 20:41:52 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Panel from './components/Panel';
 import paintings from './paintings.json';
 
 const App = () => {
-
   return (
     <div>
       <Panel title="Свежие новости">
-        <p>lorem20asdffffffffffsdfasdfasdf dfsdafasdf asdf asdfadfadfadf dfdf</p>
-        <a href=""></a>
+        <p>
+          lorem20asdffffffffffsdfasdfasdf dfsdafasdf asdf asdfadfadfadf dfdf
+        </p>
+        <a href="##"></a>
       </Panel>
       <Panel>
-        <p>фівлдаофждвілаофд ловафдао вдфілао длфова</p></Panel>
+        <p>фівлдаофждвілаофд ловафдао вдфілао длфова</p>
+      </Panel>
       <Logo text="Главный компонент" />
-      <PaintingList paintings={paintings123456}/>
+      <PaintingList paintings={paintings} />
     </div>
   );
 };
