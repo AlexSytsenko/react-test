21d064d65d1d5aaf44d38a2e377e77fb65af4466
AlexSytsenko <sytsenko@gmail.com>
2021-03-24 20:40:12 +0200

    add ColorPicker component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import Counter from './components/Counter/Counter';
+import Counter from './components/Counter';
 import Dropdown from './components/Dropdown/Dropdown';
+import ColorPicker from './components/ColorPicker';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
 
 const App = () => {
   return (
     <>
       <h1>Состояние компонента</h1>
 
-      {/* <Counter /> */}
+      <Counter />
 
       <Dropdown />
+
+      <ColorPicker options={colorPickerOptions} />
     </>
   );
 };
--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -32,18 +32,20 @@ class Counter extends React.Component {
   };
 
   render() {
+    const { value } = this.state;
+
     return (
       <div className="Counter">
-        <span className="Counter__value">{this.state.value}</span>
+        <span className="Counter__value">{value}</span>
 
         {/* <div className="Counter__controls">
-          <button type="button" onClick={this.handleIncrement}>
-            Увеличить на 1
-          </button>
-          <button type="button" onClick={this.handleDecrement}>
-            Уменьшить на 1
-          </button>
-        </div> */}
+            <button type="button" onClick={this.handleIncrement}>
+                Увеличить на 1
+            </button>
+            <button type="button" onClick={this.handleDecrement}>
+                Уменьшить на 1
+            </button>
+            </div> */}
         <Controls
           onIncrement={this.handleIncrement}
           onDecrement={this.handleDecrement}
