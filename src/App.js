42bbc294184b0618c2c9c940678b89fd39f0732c
AlexSytsenko <sytsenko@gmail.com>
2021-03-24 16:31:00 +0200

    add Counter component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import Logo from './components/Logo';
-import PaintingList from './components/PaintingList';
-import Panel from './components/Panel';
-import paintings from './paintings.json';
+import Counter from './components/Counter/Counter';
 
 const App = () => {
   return (
-    <div>
-      <Panel title="Свежие новости">
-        <p>
-          lorem20asdffffffffffsdfasdfasdf dfsdafasdf asdf asdfadfadfadf dfdf
-        </p>
-        <a href="##"></a>
-      </Panel>
-      <Panel>
-        <p>фівлдаофждвілаофд ловафдао вдфілао длфова</p>
-      </Panel>
-      <Logo text="Главный компонент" />
-      <PaintingList paintings={paintings} />
-    </div>
+    <>
+      <h1>Состояние компонента</h1>
+
+      <Counter initialValue={10} />
+    </>
   );
 };
 
